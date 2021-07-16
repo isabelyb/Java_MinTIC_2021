e5512889cfc9fa3a9d746339acf303988e0f4297
isabelyb <isabelcristinayepesb@gmail.com>
2021-07-16 10:10:54 -0500

    Starting challenge 1

---
+++ 18_challenge1/src/ProyectoFiduciaria.java
@@ -0,0 +1,11 @@
+public class ProyectoFiduciaria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    public static void diferenciaInteresSimpleYCompuesto(int pTiempo, double pMonto, double
+    pInteres) throws Exception {
+    
+    }
+}
